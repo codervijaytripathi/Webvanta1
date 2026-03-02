905ac5fdb323631ba5008b98bf0d96b3d13303a3
VIJAY TRIPATHI <vijaytripathiofficial@gmail.com>
2026-03-02 16:46:50 +0530

    Update footer.js

--- public/assets/js/footer.js
+++ public/assets/js/footer.js
@@ -1,5 +1,10 @@
-fetch('footer.html')
-  .then(res => res.text())
+
+fetch('../footer.html')
+  .then(response => {
+    if (!response.ok) throw new Error('Footer not found');
+    return response.text();
+  })
   .then(html => {
     document.getElementById('footer-placeholder').innerHTML = html;
-  });
+  })
+  .catch(err => console.error('Footer load error:', err));
